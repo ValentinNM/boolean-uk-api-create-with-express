9aaa9f8b69297b86805f1638fe0367932e62a2a4
ValentinNM <57604614+ValentinNM@users.noreply.github.com>
2021-10-08 13:13:38 +0300

    addOnePet completed

--- src/resources/pets/controller.js
+++ src/resources/pets/controller.js
@@ -7,8 +7,25 @@ Pet();
 const router = require("./router");
 
 function addOnePet(req, res) { 
-    console.log("inisde addNewPet: ", req.body);
-    res.json({dv : true})
+    // console.log("inisde addNewPet: ", req.body);
+    // res.json({dv : true})
+
+    const petToAdd = {
+        ...req.body
+    };
+
+    console.log("petToAdd: ", petToAdd);
+
+    const creatingPet = `
+        INSERT INTO pets (name, age, type, breed, microchip)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *;
+    `;
+
+    const { name, age, type, breed, microchip } = petToAdd;
+    db.query(creatingPet, [name, age, type, breed, microchip])
+    .then((result) => res.json({data : result.rows[0]}))
+    .catch(console.error);
 }
 
 module.exports = {addOnePet}
